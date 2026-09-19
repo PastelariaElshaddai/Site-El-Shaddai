4a98986602ba498c145bd383e4a08d28a36de9ea
PastelariaElshaddai <pastelaria.elshaddai.2026@gmail.com>
2026-09-19 11:46:18 -0300

    Delete BLOCO_TAMANHOS_SQL.sql

--- BLOCO_TAMANHOS_SQL.sql
+++
@@ -1,7 +0,0 @@
--- Pastelaria El Shaddai - Bloco Tamanhos
-alter table public.produtos
-  add column if not exists tamanhos jsonb default '[]'::jsonb;
-
-update public.produtos
-set tamanhos = '[]'::jsonb
-where tamanhos is null;
